1cf28bf3e5a618dd764cdfa9f7a876fc4799558c
Kamilla <ustkamilla@mail.ru>
2022-12-14 14:41:09 +0500

    create program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FirstArray = new string [5] {"func", "852", "ref", "final", "12"};
+string[] SecondArray = new string[FirstArray.Length];
+
+int count = 0;
+for (int i = 0; i < FirstArray.Length; i++)
+{
+    if(FirstArray[i].Length <= 3)
+    {
+        SecondArray[count] = FirstArray[i];
+        count++;
+        }
+    }
+
+for (int i = 0; i < SecondArray.Length; i++)
+{
+    Console.Write($"{SecondArray[i]} ");
+    }
+Console.WriteLine();
